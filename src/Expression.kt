178a47ecc97b8730dedf1ccdf1645b15feda1bca
sivabalivada <sivagopal.b@gmail.com>
2018-11-15 23:25:27 +0000

    Hello world first program -change5

---
+++ src/Expression.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var firstNum = 5
+    var secondNum = 10
+    var bigNum= if (firstNum > secondNum) {
+        firstNum
+    } else {
+        secondNum
+    }
+    println(bigNum)
+}
