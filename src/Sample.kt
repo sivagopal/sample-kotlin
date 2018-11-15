46dce532bcaf88e6431dde0394163d47bb3177b9
sivabalivada <sivagopal.b@gmail.com>
2018-11-15 23:15:38 +0000

    Hello world first program -change4

--- src/Sample.kt
+++ src/Sample.kt
@@ -1,5 +1,7 @@
 fun main(args:Array<String>) {
-    var length = 5
+    val length = 5
+    // this is not allowed as val is final length=10
     var breadth=10
-    println("Area"+ 5*10)
+    // This is allowed as var is mutanle breadth=12
+    println("Area ${length*breadth}")
 }
